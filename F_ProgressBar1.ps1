e23ef78cabfa20ce984979a5296fe87c582b7c41
lolidamour <64636627+lolidamour@users.noreply.github.com>
2021-11-20 23:55:05 +0100

    Update F_ProgressBar1.ps1

--- F_ProgressBar1.ps1
+++ F_ProgressBar1.ps1
@@ -1,10 +1,10 @@
 #Bar de progression dans une boucle Foreach
 #Avant la boucle Foreach il faut placer et initialiser une variable $i = 0
 Function ProgressBar {
-  param([$String]$ListeCount,
+  param([String]$ListCount,
       [String]$TitreProgress)
       
   $PercentComplete = [System.Math]::Round($($i*100/($ListCount)),2)
-  Write-Progress -Activity $Titre -status "Effectué : $PercentComplete %" -percentcomplete $($i*100/($ListCount))
+  Write-Progress -Activity $TitreProgress -status "Effectué : $PercentComplete %" -percentcomplete $($i*100/($ListCount))
   $i++
 }
